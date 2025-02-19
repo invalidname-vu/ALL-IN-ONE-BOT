40bbb3237b7dfb414db77b6c3fca34604d8e60d9
invalidname-vu <68509631+invalidname-vu@users.noreply.github.com>
2025-02-19 21:43:40 +0700

    Update welcomecards.js

--- UI/banners/welcomecards.js
+++ UI/banners/welcomecards.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const data = {
     welcomeImages: [
-        path.resolve(__dirname, '../welcomeimages/1.png'),
-        path.resolve(__dirname, '../welcomeimages/2.jpg'),
-        path.resolve(__dirname, '../welcomeimages/3.jpg'),
+        path.resolve(__dirname, '../welcomeimages/1.jpg'),
+        path.resolve(__dirname, '../welcomeimages/2.png'),
+        path.resolve(__dirname, '../welcomeimages/3.png'),
     ],
 };
 
